6e8500d15ba30b5c315ee359c2033a009dbbcc02
AJL <angela@AJL-MacBook-Pro.local>
2024-10-15 23:49:48 -0500

    django course 31 commit

--- profileapp/models.py
+++ profileapp/models.py
@@ -5,8 +5,8 @@ from django.db import models
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile') #user 탈퇴하면 계정 사라짐
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile') #user 탈퇴 후 계정 사라짐
 
-    image = models.ImageField(dupload_to='profile/', null = True) # 프로필 사진 없어도 괜춘
+    image = models.ImageField(upload_to='profile/', null = True) # 프로필 사진 없어도 괜춘
     nickname = models.CharField(max_length =20, unique=True, null = True)
     message = models.CharField(max_length=100, null = True)
