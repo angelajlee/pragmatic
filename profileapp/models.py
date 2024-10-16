51072f245a1084286e47f372ad106e613ca907e2
AJL <angela@AJL-MacBook-Pro.local>
2024-10-15 21:38:56 -0500

    django course 29 commit

---
+++ profileapp/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile') #user 탈퇴하면 계정 사라짐
+
+    image = models.ImageField(dupload_to='profile/', null = True) # 프로필 사진 없어도 괜춘
+    nickname = models.CharField(max_length =20, unique=True, null = True)
+    message = models.CharField(max_length=100, null = True)
