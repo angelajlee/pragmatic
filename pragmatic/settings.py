2b8d0e42804c88c14de2883d932d5df8d3fd5068
AJL <angela@AJL-MacBook-Pro.local>
2024-10-22 13:32:57 -0500

    django course 39 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] #모든 host에 대한 허용
 
 
 # Application definition
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
     "accountapp",
     "profileapp",
     "articleapp",
-    "a",
+    "commentapp",
 ]
 
 MIDDLEWARE = [
